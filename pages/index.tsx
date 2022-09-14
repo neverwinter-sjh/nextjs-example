d90211732cb0f8e685b59f0b7cb540eac9fd82d1
neverwinter-sjh <neverwinter.sjh@gmail.com>
2022-09-14 17:44:50 +0900

    iframe ref로 설정해서 onload 이벤트 실행. 이후 postmessage로 메세지 보냄

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
+import { SyntheticEvent, useRef, useEffect } from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useRef } from 'react';
 
 const Home: NextPage = () => {
   const iframe = useRef<HTMLIFrameElement>(null);
 
-  const onLoadHandler = (e: any) => {
-    console.group('Iframe Loaded');
-    console.log('e', e);
-    console.log('iframe', iframe);
-    iframe.current.contentWindow.postMessage('iframe loaded', '*');
-    console.groupEnd();
-  };
+  useEffect(() => {
+    return () => {
+      if (iframe.current) {
+        if (iframe.current.contentWindow) {
+          iframe.current.contentWindow.postMessage('test', '*');
+        }
+      }
+    };
+  }, [iframe]);
+
   return (
-    <div className="module">
-      <div
-        className="inner-container"
-        style={{ padding: '50px' }}
-      >
-        <iframe
-          ref={iframe}
-          src={'http://10.155.255.15:4001/damage/login?locale=en&systemId=sixtLeasing'}
-          style={{
-            width: '800px',
-            height: '550px',
-          }}
-          onLoad={e => onLoadHandler(e)}
-        ></iframe>
-      </div>
-    </div>
+    <iframe
+      ref={iframe}
+      src={'http://localhost:4001/damage/login?locale=en&systemId=sixtLeasing'}
+      style={{
+        position: 'fixed',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        width: '100%',
+        height: '100%',
+      }}
+    ></iframe>
   );
 };
 
